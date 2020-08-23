58a33423a2ffc8681a8eb0898d40eca16e0e051a
lcarv20 <lcarv20@gmail.com>
2020-08-24 01:29:21 +0200

    Testing meaningcloud API with simple fetch

--- src/server/server.js
+++ src/server/server.js
@@ -2,13 +2,34 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const fetch = require("node-fetch")
+
 require("dotenv").config({ debug: process.env.DEBUG })
 
 const port = process.env.PORT
+const apiKey = process.env.API_KEY
+const tempUrl =
+	"https://examples.yourdictionary.com/descriptive-text-examples.html"
 
 //Dependecies
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+//MeaningCloud API Settings
+
+var options = {
+	method: "POST",
+	hostname: "api.meaningcloud.com",
+	path: `/sentiment-2.1?key=${apiKey}&lang=auto&url="${tempUrl}"&model=general`,
+	headers: {},
+	maxRedirects: 20,
+}
+
+fetch(
+	`https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&lang=auto&url=${tempUrl}&model=general`
+)
+	.then((data) => data.json())
+	.then((data) => console.log(data))
 
 //************ TESTING SITE ************
 //For testing purposes we'll serve directly from the src folder
